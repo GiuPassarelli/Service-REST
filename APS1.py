a11b6c1f6828f41410320b23b28beb248d6fe3b6
GiuPassarelli <gb_passarelli@hotmail.com>
2019-11-08 12:04:51 -0300

    APS2

--- APS1.py
+++ APS1.py
@@ -1,17 +1,13 @@
 from flask import Flask
 from flask_restful import Resource, Api, reqparse
 
-app = Flask(__name__)
-api = Api(app)
+dict_tarefas = {}
 
-dict_tarefas = {
-    1: {'tarefa': 'tarefa1'},
-    2: {'tarefa': 'tarefa2'},
-    3: {'tarefa': 'tarefa3'},
-}
+id_count = 0
 
 parser = reqparse.RequestParser()
-parser.add_argument('tarefa')
+parser.add_argument('titulo', location='json')
+parser.add_argument('texto', location='json')
 
 class Lista_Tarefas(Resource):
     def get(self):
@@ -19,9 +15,10 @@ class Lista_Tarefas(Resource):
 
     def post(self):
         args = parser.parse_args()
-        id_tarefa = max(dict_tarefas.keys()) + 1
-        dict_tarefas[id_tarefa] = {'tarefa': args['tarefa']}
-        return dict_tarefas[id_tarefa], 201
+        global id_count
+        id_count += 1
+        dict_tarefas[id_count] = {'titulo': args['titulo'], 'texto': args['texto']}
+        return dict_tarefas[id_count], 201
 
 class Tarefas(Resource):
     def get(self, id_tarefa):
@@ -29,21 +26,10 @@ class Tarefas(Resource):
 
     def put(self, id_tarefa):
         args = parser.parse_args()
-        tarefa = {'tarefa': args['tarefa']}
+        tarefa = {'titulo': args['titulo'], 'texto': args['texto']}
         dict_tarefas[int(id_tarefa)] = tarefa
         return tarefa, 201
 
     def delete(self, id_tarefa):
-        del dict_tarefas[4]
+        del dict_tarefas[int(id_tarefa)]
         return '', 204
-
-class Healthcheck(Resource):
-    def get(self):
-    	return '', 200
-
-api.add_resource(Lista_Tarefas, '/Tarefa')
-api.add_resource(Tarefas, '/Tarefa/<id_tarefa>')
-api.add_resource(Healthcheck, '/healthcheck')
-
-if __name__ == '__main__':
-    app.run(debug=True)
---
+++ webserver.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask_restful import Api
+from APS1 import Lista_Tarefas, Tarefas
+
+app = Flask(__name__)
+api = Api(app)
+
+api.add_resource(Lista_Tarefas, '/Tarefa')
+api.add_resource(Tarefas, '/Tarefa/<id_tarefa>')
+
+@app.route('/healthcheck')
+def Healthcheck():
+    return '', 200
+
+if __name__ == '__main__':
+    app.run(debug=True)
