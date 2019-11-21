0a209ddf92003fc67194395275fbf48d1ce6ff70
GiuPassarelli <gb_passarelli@hotmail.com>
2019-11-21 15:35:19 -0300

    sh yet again

--- install.sh
+++ install.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+echo "start'"
 sudo apt update -y;
+echo 111
 sudo apt install python3-pip -y;
 pip3 install Flask;
+echo 222
 pip3 install flask_restful;
+echo 123
 tmux new -d -s server_tmux 'python3 webserver.py;';
