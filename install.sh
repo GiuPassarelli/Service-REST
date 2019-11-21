c68b12aa4cdf133187244e8a259b03abcfc4991a
GiuPassarelli <gb_passarelli@hotmail.com>
2019-11-21 15:27:59 -0300

    seila

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-sudo apt update -y
-sudo apt install python3-pip -y
-pip3 install Flask
-pip3 install flask_restful
-tmux new -d -s server_tmux 'python3 webserver.py;' 
+sudo apt update -y;
+sudo apt install python3-pip -y;
+pip3 install Flask;
+pip3 install flask_restful;
+tmux new -d -s server_tmux 'python3 webserver.py;';
