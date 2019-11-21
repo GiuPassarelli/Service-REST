cda7d8facefacc855927957a251551a568b072bf
GiuPassarelli <gb_passarelli@hotmail.com>
2019-11-21 14:26:22 -0300

    -y to install

--- install.sh
+++ install.sh
@@ -1,6 +1,5 @@
-cd Service-REST
-sudo apt update
-sudo apt install python3-pip
+sudo apt update -y
+sudo apt install python3-pip -y
 pip3 install Flask
 pip3 install flask_restful
 python3 webserver.py
